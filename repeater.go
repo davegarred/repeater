79dd55df1ce3cbfbbc2732f3b7575d3f1263c4e3
Dave Garred <dave.garred@gmail.com>
2018-01-15 04:59:08 +0000

    use path matching

--- repeater.go
+++ repeater.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"path"
 
 	"github.com/davegarred/repeater/persist"
 )
@@ -16,11 +17,35 @@ func retrieveHandler(w http.ResponseWriter, r *http.Request) {
 	configuredRetrieveHandler(w, r, store)
 }
 
+type pathResolver struct {
+	handlers map[string]http.HandlerFunc
+}
+
+func (p *pathResolver) Add(path string, handler http.HandlerFunc) {
+	p.handlers[path] = handler
+}
+
+func (p *pathResolver) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	signature := r.Method + " " + r.URL.Path
+	for pattern, handler := range p.handlers {
+		if ok, err := path.Match(pattern, signature); ok && err == nil {
+			handler(w, r)
+			return
+		} else if err != nil {
+			panic(err)
+		}
+		fmt.Printf("missed pattern: %v\n", pattern)
+	}
+	fmt.Printf("missed grab:    %v\n", signature)
+	http.NotFound(w, r)
+}
+
 func main() {
 	store = persist.NewStore()
-	http.HandleFunc("/store", storeHandler)
-	http.HandleFunc("/retrieve/", retrieveHandler)
-	err := http.ListenAndServe(":8000", nil)
+	pathResolver := &pathResolver{handlers: make(map[string]http.HandlerFunc)}
+	pathResolver.Add("GET /store", storeHandler)
+	pathResolver.Add("GET /retrieve/*", retrieveHandler)
+	err := http.ListenAndServe(":8000", pathResolver)
 	if err != nil {
 		fmt.Printf("%v\n", err)
 	}
